1197d8aaf2577b989653de98a85af4d8c5b00af7
Victor Meunier <mreliptik@pop-os.localdomain>
2019-06-27 08:09:03 +0200

    update

--- mosaic.py
+++ mosaic.py
@@ -1,17 +1,26 @@
-import cv2 as cv
 import os
 import random
+import cv2 as cv
+import numpy as np
 
 target_path = 'images/target/lena.jpg'
-input_path = ''
-
-target_im = cv.imread(target_path)
+input_path = 'images/input/'
+image_extensions = ('.png', '.jpg', '.jpeg', '.jfiff', '.tiff', '.bmp')
 input_files = []
 
-input_files_size = 20
+target_im = cv.imread(target_path)
 
-# resize
-scale_percent = 40 # percent of original size
+for subdir, _, files in os.walk(input_path):
+    print('[INFO] Working on: ' + str(subdir))
+    for _file in files:
+        if str(_file).lower().endswith(image_extensions):
+            input_files.append(os.path.join(subdir, _file))
+
+input_files_size = len(input_files)
+print('[INFO] Found {} input files.'.format(input_files_size))
+
+# resize params
+scale_percent = 45 # percent of original size
 width = int(target_im.shape[1] * scale_percent / 100)
 height = int(target_im.shape[0] * scale_percent / 100)
 dim = (width, height)
@@ -28,10 +37,51 @@ for i in range(row):
     for j in range(col):
         color = (random.random()*255, random.random()*255, random.random()*255)
         if i == 0:
-            cv.rectangle(resized, (i*kernel_size, j*kernel_size), (kernel_size+(i*kernel_size), kernel_size+(j*kernel_size)), color, 1)
+            x1 = i*kernel_size
+            y1 = j*kernel_size
+            x2 = kernel_size+(i*kernel_size)
+            y2 = kernel_size+(j*kernel_size)
+            cv.rectangle(resized, (x1, y1), (x2, y2), color, 1)
+            print(np.mean(resized[x1:x2, y1:y2,:]))
         else:
-            cv.rectangle(resized, (1+(i*kernel_size), 1+(j*kernel_size)), (kernel_size+(i*kernel_size), kernel_size+(j*kernel_size)), color, 1)
+            x1 = 1+(i*kernel_size)
+            y1 = 1+(j*kernel_size)
+            x2 = kernel_size+(i*kernel_size)
+            y2 = kernel_size+(j*kernel_size)
+            cv.rectangle(resized, (x1, y1), (x2, y2), color, 1)
+            print(np.mean(resized[x1:x2, y1:y2,:]))
 cv.imshow('lena', resized)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
+
+def getDominantColor():
+    from __future__ import print_function
+    import binascii
+    import struct
+    from PIL import Image
+    import numpy as np
+    import scipy
+    import scipy.misc
+    import scipy.cluster
+
+    NUM_CLUSTERS = 5
+
+    print('reading image')
+    im = Image.open('image.jpg')
+    im = im.resize((150, 150))      # optional, to reduce time
+    ar = np.asarray(im)
+    shape = ar.shape
+    ar = ar.reshape(scipy.product(shape[:2]), shape[2]).astype(float)
+
+    print('finding clusters')
+    codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
+    print('cluster centres:\n', codes)
+
+    vecs, dist = scipy.cluster.vq.vq(ar, codes)         # assign codes
+    counts, bins = scipy.histogram(vecs, len(codes))    # count occurrences
+
+    index_max = scipy.argmax(counts)                    # find most frequent
+    peak = codes[index_max]
+    colour = binascii.hexlify(bytearray(int(c) for c in peak)).decode('ascii')
+    print('most frequent is %s (#%s)' % (peak, colour))
