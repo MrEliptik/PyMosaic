2a4fd4ca92bc6d5302ba51086c99e34fd05122c5
MrEliptik <victormeunier.dev@gmail.com>
2019-06-30 10:11:09 +0200

    delete timeit function

--- mosaic.py
+++ mosaic.py
@@ -18,21 +18,6 @@ from multiprocessing.pool import Pool
 
 import time
 
-def timeit(method):
-    def timed(*args, **kw):
-        ts = time.time()
-        result = method(*args, **kw)
-        te = time.time()        
-        if 'log_time' in kw:
-            name = kw.get('log_name', method.__name__.upper())
-            kw['log_time'][name] = int((te - ts) * 1000)
-        else:
-            print('%r  %2.2f ms' % \
-                  (method.__name__, (te - ts) * 1000))
-        return result   
-    return timed
-
-
 def resize(image, factor):
     width = int(image.shape[1] * factor)
     height = int(image.shape[0] * factor)
